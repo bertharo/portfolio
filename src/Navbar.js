74495e3712a8abf9e60302729468f0a50ff97b00
bertharo <bertharo23@gmail.com>
2025-07-15 15:35:09 -0700

    Remove hamburger menu for cleaner minimalistic navigation

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
   return (
     <nav className={styles.navbar}>
-      <button
-        className={styles.menuButton}
-        onClick={() => setOpen((prev) => !prev)}
-        aria-label="Toggle navigation menu"
-      >
-        <span className={styles.menuIcon}>&#9776;</span>
-      </button>
-      <ul className={`${styles.navList} ${open ? styles.open : ''}`}>
+      <ul className={styles.navList}>
         <li><a className={styles.navLink} href="#about">About</a></li>
         <li><a className={styles.navLink} href="#featured">Featured</a></li>
         <li><a className={styles.navLink} href="#projects">Projects</a></li>
